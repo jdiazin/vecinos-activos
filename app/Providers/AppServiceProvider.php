41e9c33a50e64aa83b1e4eb308a021eb5f2e234a
Jeremy Diaz <diazmosisjeremy@gmail.com>
2026-08-31 20:26:58 -0400

    feat: integrar migraciones automaticas en AppServiceProvider para produccion

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,8 @@ use Illuminate\Auth\Events\Login;
 use Illuminate\Auth\Events\Logout;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\App;
 use App\Helpers\AuditLogger;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +23,13 @@ class AppServiceProvider extends ServiceProvider
         // Forzar HTTPS en producción para que carguen bien los assets (Vite)
         if (env('APP_ENV') === 'production') {
             URL::forceScheme('https');
+            
+            // Ejecutar migraciones automáticamente al arrancar en producción (Render)
+            try {
+                Artisan::call('migrate', ['--force' => true]);
+            } catch (\Exception $e) {
+                // Previene que falle el arranque si la base de datos no está lista de inmediato
+            }
         }
 
         Event::listen(Login::class, function ($event) {
